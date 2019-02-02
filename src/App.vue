2ec0658632a72de125b2f14afce8a38f95cd8dcd
Sergius <ceo@frontup.ru>
2019-02-02 09:50:56 +0500

    git init

---
+++ src/App.vue
@@ -0,0 +1,106 @@
+<template>
+  <div id="app">
+    <el-row>
+      <el-col :xl="{span:6, offset: 8}" :lg="{span:10, offset: 6}" justify="center">
+        <img alt="Vue logo" src="./assets/logo.png" style="center">
+        <h1>Анкета постоянного покупателя</h1>
+        <el-form ref="form" :model="form">
+          <el-form-item label="Введите номер карты" required>
+            <el-input v-model="form.card" autofocus :readonly="secondBlock"></el-input>
+          </el-form-item>
+          <el-form-item v-show="!secondBlock">
+            <el-button type="primary" @click="checkCard">Далее</el-button>
+          </el-form-item>
+
+          <div v-show="secondBlock">
+            <el-row :gutter="10">
+              <el-col :span="24">
+                <el-form-item label="Фамилия" required>
+                  <el-input type="surname" v-model="form.surname"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="Имя" required>
+                  <el-input type="name" v-model="form.name"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="Отчество">
+                  <el-input type="middlename" v-model="form.middlename"></el-input>
+                </el-form-item>
+              </el-col>
+
+              <el-col :span="24">
+                <el-form-item label="День рождения" required>
+                  <el-date-picker v-model="form.birthday" type="date" style="width: 100%;"></el-date-picker>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="Телефон" required>
+                  <el-input type="tel" v-model="form.phone">
+                    <span slot="prepend">+7</span>
+                  </el-input>
+                </el-form-item>
+              </el-col>
+              <el-col :span="12">
+                <el-form-item label="Email" required>
+                  <el-input type="email" v-model="form.email"></el-input>
+                </el-form-item>
+              </el-col>
+              <el-form-item label required>
+                <el-checkbox
+                  v-model="form.spamAgree"
+                  style="float:left"
+                >Клиент согласен на получение рассылки?</el-checkbox>
+              </el-form-item>
+
+              <el-form-item>
+                <el-button @click="clear">Сбросить</el-button>
+                <el-button type="primary" @click="validate">Записать</el-button>
+              </el-form-item>
+            </el-row>
+          </div>
+        </el-form>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+
+  data() {
+    return {
+      secondBlock: false,
+      form: {
+        сard: "",
+        surname: "",
+        middlename: "",
+        name: "",
+        birthday: "",
+        email: "",
+        phone: "",
+        spamAgree: true
+      }
+    };
+  },
+  methods: {
+    checkCard: function() {
+      this.secondBlock = true;
+    },
+    validate: function() {},
+    clear: function() {}
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/main.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App.vue'
+import ElementUI from 'element-ui'
+import lang from 'element-ui/lib/locale/lang/ru-RU'
+import locale from 'element-ui/lib/locale'
+import 'element-ui/lib/theme-chalk/index.css'
+locale.use(lang)
+Vue.use(ElementUI)
+
+
+Vue.config.productionTip = false
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
