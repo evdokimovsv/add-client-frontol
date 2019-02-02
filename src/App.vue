5e10969c0de62889e902c5b8fd45c368901fda68
Sergius <sergius@localhost.localdomain>
2019-02-02 15:39:58 +0500

    add: validate all fields

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,25 @@
     <el-row>
       <el-col :xl="{span:10, offset: 6}" :lg="{span:18, offset: 3}" justify="center">
         <img alt="Vue logo" src="./assets/logo.png" style="center">
-        <h1>Анкета постоянного покупателя</h1>
-        <el-form ref="form" :model="form">
+        <h1>Электронная анкета постоянного покупателя</h1>
+        <el-form
+          :model="form"
+          ref="form"
+          @validate="validate"
+          :rules="rules"
+          @submit.native.prevent
+        >
           <el-row :gutter="10">
             <el-col :span="8" :offset="8">
-              <el-form-item label="Введите номер карты" required>
-                <el-input v-model="form.card" autofocus :disabled="secondBlock"></el-input>
+              <el-form-item label="Номер штрих-кода карты" required prop="card" :rules="rules.card">
+                <el-input
+                  v-model="form.card"
+                  autofocus
+                  :disabled="secondBlock"
+                  @keyup.enter.native="checkCard"
+                >
+                  <template slot="prepend">2501723</template>
+                </el-input>
               </el-form-item>
               <el-form-item v-show="!secondBlock">
                 <el-button type="primary" @click="checkCard">Далее</el-button>
@@ -18,36 +31,41 @@
           <div v-show="secondBlock">
             <el-row :gutter="10">
               <el-col :span="8">
-                <el-form-item label="Фамилия" required style>
+                <el-form-item label="Фамилия" required prop="surname" :rules="rules.surname">
                   <el-input type="surname" v-model="form.surname"></el-input>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
-                <el-form-item label="Имя" required>
+                <el-form-item label="Имя" required prop="name" :rules="rules.name">
                   <el-input type="name" v-model="form.name"></el-input>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
-                <el-form-item label="Отчество">
+                <el-form-item label="Отчество" prop="middlename" :rules="rules.middlename">
                   <el-input type="middlename" v-model="form.middlename"></el-input>
                 </el-form-item>
               </el-col>
             </el-row>
             <el-row :gutter="10">
               <el-col :span="8">
-                <el-form-item label="День рождения" required>
-                  <el-date-picker v-model="form.birthday" type="date" style="width: 100%;"></el-date-picker>
+                <el-form-item
+                  label="День рождения"
+                  required
+                  prop="birthday"
+                  :rules="rules.birthday"
+                >
+                  <el-date-picker v-model="form.birthday" type="date" style="width: 100%;" default-value="1990-10-01"></el-date-picker>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
-                <el-form-item label="Телефон" required>
+                <el-form-item label="Телефон" required prop="phone" :rules="rules.phone">
                   <el-input type="tel" v-model="form.phone">
-                    <span slot="prepend">+7</span>
+                    <template slot="prepend">+7</template>
                   </el-input>
                 </el-form-item>
               </el-col>
               <el-col :span="8">
-                <el-form-item label="Email" required>
+                <el-form-item label="Email" required prop="email" :rules="rules.email">
                   <el-input type="email" v-model="form.email"></el-input>
                 </el-form-item>
               </el-col>
@@ -56,7 +74,7 @@
               <el-checkbox v-model="form.spamAgree">Клиент согласен на получение рассылки?</el-checkbox>
             </el-form-item>
             <el-form-item>
-              <el-button @click="clear">Сбросить</el-button>
+              <el-button @click="reset">Сбросить</el-button>
               <el-button type="primary" @click="validate">Записать</el-button>
             </el-form-item>
           </div>
@@ -81,15 +99,104 @@ export default {
         email: "",
         phone: "",
         spamAgree: true
+      },
+      rules: {
+        card: [
+          {
+            required: true,
+            message: "Пожалуйста, введите номер карты",
+            trigger: "blur"
+          },
+          {
+            min: 6,
+            max: 6,
+            message: "Длинна поля должна быть 6 символов",
+            trigger: "blur"
+          }
+        ],
+        surname: [
+          {
+            required: true,
+            message: "Пожалуйста, введите фамилию",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            pattern: "^[а-яА-Я]",
+            message: "Язык ввода должен быть русским",
+            trigger: "blur"
+          }
+        ],
+        name: [
+          {
+            required: true,
+            message: "Пожалуйста, введите имя",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            pattern: "^[а-яА-Я]",
+            message: "Язык ввода должен быть русским",
+            trigger: "blur"
+          }
+        ],
+        middlename: [
+          {
+            required: true,
+            message: "Пожалуйста, введите отчетсво",
+            trigger: "blur"
+          },
+          {
+            type: "string",
+            pattern: "^[а-яА-Я]",
+            message: "Язык ввода должен быть русским",
+            trigger: "blur"
+          }
+        ],
+        birthday: [
+          {
+            required: true,
+            message: "Пожалуйста, введите день рождение",
+            trigger: "blur"
+          }
+        ],
+        phone: [
+          {
+            required: true,
+            message: "Пожалуйста, введите телефон",
+            trigger: "blur"
+          },
+          {
+            min: 10,
+            max: 10,
+            message: "Длинна поля должна быть 10 символов",
+            trigger: "blur"
+          }
+        ],
+        email: [
+          {
+            required: true,
+            type: "email",
+            message: "Пожалуйста, введите  корректный email",
+            trigger: "blur"
+          }
+        ]
       }
     };
   },
   methods: {
     checkCard: function() {
-      this.secondBlock = true;
+      this.$refs.form.validateField("card", error => {
+        if (!error) {
+          this.secondBlock = true;
+        }
+      });
     },
     validate: function() {},
-    clear: function() {}
+    reset: function() {
+      this.$refs["form"].resetFields();
+      this.secondBlock = false;
+    }
   }
 };
 </script>
--- src/main.js
+++ src/main.js
@@ -5,8 +5,14 @@ import ElementUI from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/ru-RU'
 import locale from 'element-ui/lib/locale'
 import 'element-ui/lib/theme-chalk/index.css'
+
+
+
 locale.use(lang)
 Vue.use(ElementUI)
+
+
+
 
 
 Vue.config.productionTip = false
